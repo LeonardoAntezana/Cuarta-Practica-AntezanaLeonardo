437bc5ecbd471ec15afc17f01b41d31600794af9
¨Leonardo <leonardoantezana59@gmail.com>
2023-07-07 12:49:05 -0300

    creando conexion con base de datos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import handlebars from 'express-handlebars'
 import __dirname from './utils.js';
 import { Server } from 'socket.io';
-import { ProductManager } from './ProductManager.js'
-import { CartManager } from './CartManager.js';
+import { ProductManager } from './dao/fileManagers/ProductManager.js';
+import { CartManager } from './dao/fileManagers/CartManager.js';
 import productsRouter from './routes/router.products.js'
 import cartRouter from './routes/router.cart.js'
 import viewRouter from './routes/router.views.js'
@@ -11,9 +12,15 @@ import viewRouter from './routes/router.views.js'
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 const httpServer = app.listen(8080, console.log('Server arriba'))
 export const socketServer = new Server(httpServer);
+
+mongoose.connect('mongodb+srv://leonardoantezana59:zHXuMizLukj6R75z@ecommerce-coderhouse.sfxkmnu.mongodb.net/?retryWrites=true&w=majority').                   //CONNECTION DATABASE//
+  catch(error => {
+    console.log(error)
+    process.exit();
+  });
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`)
