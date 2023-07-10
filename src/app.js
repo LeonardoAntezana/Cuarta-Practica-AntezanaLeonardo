667b7adc270513ee3cde2e81c45f3c6181c2426a
¨Leonardo <leonardoantezana59@gmail.com>
2023-07-10 12:34:11 -0300

    creando instancias de clases db

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 import handlebars from 'express-handlebars'
 import __dirname from './utils.js';
-import { Server } from 'socket.io';
+
 import { ProductManager } from './dao/fileManagers/ProductManager.js';
 import { CartManager } from './dao/fileManagers/CartManager.js';
+
+import Messages from './dao/dbManagers/message.js';
+import Products from './dao/dbManagers/product.js';
+import Carts from './dao/dbManagers/cart.js'
+
 import productsRouter from './routes/router.products.js'
 import cartRouter from './routes/router.cart.js'
 import viewRouter from './routes/router.views.js'
@@ -13,6 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const httpServer = app.listen(8080, console.log('Server arriba'))
 export const socketServer = new Server(httpServer);
 
@@ -22,14 +29,26 @@ mongoose.connect('mongodb+srv://leonardoantezana59:zHXuMizLukj6R75z@ecommerce-co
     process.exit();
   });
 
+app.use(express.static(`${__dirname}/public`));
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars');
-app.use('/', viewRouter)
-app.use(express.static(`${__dirname}/public`));
 
 export const manager1 = new ProductManager('Leonardo', './products.json')
 export const cartManager1 = new CartManager('./carts.json');
+export const messagesManager = new Messages();
+export const productsDbManager = new Products();
+export const cartsDbManager = new Carts();
 
+app.use('/', viewRouter)
 app.use('/api/products/', productsRouter);
 app.use('/api/carts/', cartRouter)
+
+socketServer.on('connection', async (socket) => {
+
+  socket.on('sendMessage', async (data) => {
+    let res = await messagesManager.addMessage(data);
+    if(res.code !== 11000) socketServer.emit('newMessage', data);
+  })
+
+})
