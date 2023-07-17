6025c21b1ca1e49b08c8c547a55e7aa974ef2a98
¨Leonardo <leonardoantezana59@gmail.com>
2023-07-17 15:49:54 -0300

    mostrando products y cartProducts

--- src/routes/router.views.js
+++ src/routes/router.views.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { manager1 } from "../app.js";
 import { socketServer } from "../app.js";
 import { messagesManager } from "../app.js";
+import { productsDbManager } from "../app.js";
+import { cartsDbManager } from "../app.js";
 
 const router = Router();
 
@@ -13,6 +15,15 @@ router.get('/', async (req, res) => {
   });
 })
 
+router.get('/products', async (req, res) => {
+  let { page = 1 } = req.query;
+  let { docs: products, hasNextPage, hasPrevPage, prevPage, nextPage } = await productsDbManager.getAllPaginate(10, page);
+  res.render('products', {
+    style: 'products.css',
+    response: { products, hasNextPage, hasPrevPage, prevPage, nextPage }
+  })
+})
+
 router.get('/realtimeproducts', async (req, res) => {
   socketServer.on('connection', () => console.log('cliente conectado'))
   const products = await manager1.getProducts();
@@ -20,6 +31,15 @@ router.get('/realtimeproducts', async (req, res) => {
     style: 'realTimeProducts.css',
     products
   });
+})
+
+router.get('/carts/:cid', async (req, res) => {
+  let { cid } = req.params;
+  let { products } = await cartsDbManager.getOneCart(cid);
+  if (products){
+    return res.render('detailsCart', { style: 'detailsCart.css', products });
+  }
+  res.send({ status: 'No se encontro nigun carrito' })
 })
 
 router.get('/chat', async (req, res) => {
