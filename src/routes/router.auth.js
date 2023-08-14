d9e8a772a02b4f20630025700bc25e75f95ebdac
¨Leonardo <leonardoantezana59@gmail.com>
2023-08-14 09:14:55 -0300

    generando tokens

--- src/routes/router.auth.js
+++ src/routes/router.auth.js
@@ -1,33 +1,45 @@
 import { Router } from "express";
-import passport from "passport";
-import { sendPayload, sendError } from "../utils.js";
+import { sendPayload, sendError, generateToken } from "../utils.js";
+import { passportAuth } from "../config/passport.utilities.js";
 
 const router = Router();
 
 // PASSPORT GITHUB
-router.get('/github', passport.authenticate('github', { scope: ['user: email'] }), (req, res) => { })
+router.get('/github', passportAuth('github', { scope: ['user: email'] }), (req, res) => { })
 
 // CALLBACK GITHUB
-router.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
-  req.session.user = { name: req.user._doc.first_name, rol: 'user' };
+router.get('/githubcallback', passportAuth('github', { failureRedirect: '/login' }), (req, res) => {
+  
+  let { first_name, last_name, role } = req.user;
+  req.session.user = { name: `${first_name} ${last_name || ''}`, role };
+  let token = generateToken({first_name, last_name, role}, '10h');
+  res.cookie('authCookie', token, { httpOnly: true });
   res.redirect('/')
 })
 
 // REGISTER
-router.post('/register', passport.authenticate('register', { failureRedirect: '/login' }), async (req, res) => {
+router.post('/register', passportAuth('register', { failureRedirect: '/login' }), async (req, res) => {
   sendPayload(res, 200, 'Usuario registrado');
 })
 
 // LOGIN
-router.post('/login', passport.authenticate('login', { failureRedirect: '/login' }), async (req, res) => {
+router.post('/login', passportAuth('login', { failureRedirect: '/login' }), async (req, res) => {
   try {
-    let { first_name, last_name, rol } = req.user;
-    req.session.user = { name: `${first_name} ${last_name || ''}`, rol };
-    if (rol === 'admin') return sendPayload(res, 200, 'Admin logeado')
+    let { first_name, last_name, role } = req.user;
+    req.session.user = { name: `${first_name} ${last_name || ''}`, role };
+    let token = generateToken({first_name, last_name, role}, '10h');
+    res.cookie('authCookie', token, { httpOnly: true });
+    if (role === 'admin') return sendPayload(res, 200, 'Admin logeado')
     sendPayload(res, 200, 'Usuario logeado')
   } catch (error) {
     sendError(res, 400, error);
   }
+})
+
+// STRATEGY JWT
+
+router.get('/current', passportAuth('current', { session: false }), (req, res) => {
+  res.send({ payload: req.user })
 })
 
 // LOG0UT
@@ -36,6 +48,7 @@ router.get('/logout', (req, res) => {
     if (err) return sendError(res, 400, err);
   });
   res.clearCookie('connect.sid')
+  res.clearCookie('authCookie')
   res.redirect('/login')
 })
 
