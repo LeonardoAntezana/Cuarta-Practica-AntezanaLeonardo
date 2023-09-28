fde255eca115f3b1aafea97cbfa5f2afdcbfea59
¨Leonardo <leonardoantezana59@gmail.com>
2023-09-28 16:21:17 -0300

    incroporando nodemailer y google keys

--- src/config/config.env.js
+++ src/config/config.env.js
@@ -14,3 +14,8 @@ export default {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   CALLBACK_URL: process.env.CALLBACK_URL
 }
+
+export const GOOGLE_AUTHS = {
+  USER_EMAIL: process.env.GOOGLE_USER_EMAIL,
+  USER_PASSWORD: process.env.GOOGLE_USER_PASSWORD
+}
