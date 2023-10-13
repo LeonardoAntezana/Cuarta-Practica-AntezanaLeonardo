09243d6c3c934b9e3855a63732bb59f26e6d00a2
¨Leonardo <leonardoantezana59@gmail.com>
2023-10-12 23:08:10 -0300

    cambiando mock de prueba

--- test/auth/auth.test.js
+++ test/auth/auth.test.js
@@ -8,11 +8,11 @@ describe('Testing Auth routes', () => {
 
   it('[POST] api/auth/register - Register a new user', async function () {
     const registerUserMock = {
-      first_name: 'Lucy',
-      last_name: 'Perez',
-      email: 'lucyperez@gmail.com',
-      age: 20,
-      password: '12345678',
+      first_name: 'Pablo',
+      last_name: 'Garcia',
+      email: 'pablogarcia@gmail.com',
+      age: 22,
+      password: '123',
     }
     const response = await requester.post('/api/auth/register').send(registerUserMock);
     expect(response.statusCode).to.be.eql(200);
@@ -21,8 +21,8 @@ describe('Testing Auth routes', () => {
 
   it('[POST] api/auth/login - Login a user', async () => {
     const loginUserMock = {
-      email: 'lucyperez@gmail.com',
-      password: '12345678',
+      email: 'pablogarcia@gmail.com',
+      password: '123',
     }
     const response = await requester.post('/api/auth/login').send(loginUserMock);
     cookie = response.headers['set-cookie'][0];
@@ -32,8 +32,8 @@ describe('Testing Auth routes', () => {
 
   it('[GET] api/auth/current - Get current user', async () => {
     const response = await requester.get('/api/auth/current').set('Cookie', cookie);
-    expect(response.body.payload.name).to.be.eql('Lucy Perez');
-    expect(response.body.payload.email).to.be.eql('lucyperez@gmail.com');
+    expect(response.body.payload.name).to.be.eql('Pablo Garcia');
+    expect(response.body.payload.email).to.be.eql('pablogarcia@gmail.com');
   })
 
 })
