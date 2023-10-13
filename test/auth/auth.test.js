b80b2ecc8ee024ba9cfd617077fe70ab0073b1cf
¨Leonardo <leonardoantezana59@gmail.com>
2023-10-12 22:59:59 -0300

    descomentando codigo

--- test/auth/auth.test.js
+++ test/auth/auth.test.js
@@ -5,19 +5,19 @@ const requester = supertest('http://localhost:8080');
 let cookie;
 
 describe('Testing Auth routes', () => {
-  
-  // it('[POST] api/auth/register - Register a new user', async function () {
-  //   const registerUserMock = {
-  //     first_name: 'Lucy',
-  //     last_name: 'Perez',
-  //     email: 'lucyperez@gmail.com',
-  //     age: 20,
-  //     password: '12345678',
-  //   }
-  //   const response = await requester.post('/api/auth/register').send(registerUserMock);
-  //   // expect(response.statusCode).to.be.eql(200);
-  //   expect(response.body.payload).to.be.eql('User registed');
-  // })
+
+  it('[POST] api/auth/register - Register a new user', async function () {
+    const registerUserMock = {
+      first_name: 'Lucy',
+      last_name: 'Perez',
+      email: 'lucyperez@gmail.com',
+      age: 20,
+      password: '12345678',
+    }
+    const response = await requester.post('/api/auth/register').send(registerUserMock);
+    expect(response.statusCode).to.be.eql(200);
+    expect(response.body.payload).to.be.eql('User registed');
+  })
 
   it('[POST] api/auth/login - Login a user', async () => {
     const loginUserMock = {
@@ -26,14 +26,12 @@ describe('Testing Auth routes', () => {
     }
     const response = await requester.post('/api/auth/login').send(loginUserMock);
     cookie = response.headers['set-cookie'][0];
-    // expect(response.statusCode).to.be.eql(200);
+    expect(response.statusCode).to.be.eql(200);
     expect(response.body.payload).to.be.eql('Usuario logeado');
   })
 
   it('[GET] api/auth/current - Get current user', async () => {
     const response = await requester.get('/api/auth/current').set('Cookie', cookie);
-    // expect(response.statusCode).to.be.eql(200);
-    console.log(response.body.payload);
     expect(response.body.payload.name).to.be.eql('Lucy Perez');
     expect(response.body.payload.email).to.be.eql('lucyperez@gmail.com');
   })
